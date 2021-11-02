2ae069445c37eafa8c00607a4f15399dea4214e9
Sarmistha Pal <sarmistha15-9306@diu.edu.bd>
2021-11-02 22:56:08 +0600

    Add files via upload

---
+++ 4. HRS - Loops.py
@@ -0,0 +1,5 @@
+n=int(input())
+i=0
+if n>0:
+    for i in range(n):
+        print(i*i)
---
+++ 5. HRS - Print Function.py
@@ -0,0 +1,6 @@
+def num(n):
+    n = int(input())
+    for i in range(n):
+        i = i+1
+        print(i, end='')
+num(1)
