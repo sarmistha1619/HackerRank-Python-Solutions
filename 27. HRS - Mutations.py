07472c1072dfef858084930393aeed827d752a47
Sarmistha Pal <sarmistha15-9306@diu.edu.bd>
2021-11-16 16:40:18 +0600

    Add files via upload

---
+++ 27. HRS - Mutations.py
@@ -0,0 +1,15 @@
+'''
+string = "abracadabra"
+l = list(string)
+string = ''.join(l)
+print(string)'''
+
+def mutate_string(string, position, character):
+    res = string[:position]+character+string[position+1:]
+    return res
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
